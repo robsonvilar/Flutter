7afe279feb6d6b65bbca87e8fbd73793a008bfc8
Robson Vilar <61123228+robsonvilar@users.noreply.github.com>
2024-01-18 19:05:00 -0300

    Add files via upload

---
+++ código calculadora projeto/lib/main.dart
@@ -0,0 +1,392 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+/*
+Para fazer esse código funcionar, adicione o código ao arquivo pubspec.yaml
+
+dev_dependencies:
+  math_expressions: ^2.0.1
+
+*/
+
+double funcao_eval(String entrada) {
+  Parser p = Parser();
+  Expression exp = p.parse(entrada);
+
+  ContextModel cm = ContextModel();
+  double resultado = exp.evaluate(EvaluationType.REAL, cm);
+
+  return resultado;
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String variavel_exibe_calculo = '';
+  String variavel_exibe_resultado_do_calculo = '';
+
+  List<String> lista_termos_operacionais = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '.',
+    '+',
+    '-',
+    '/',
+    'x',
+    '(',
+    ')'
+  ];
+
+  void funcao_atualiza_displays() {
+    setState(() {
+      variavel_exibe_calculo;
+      variavel_exibe_resultado_do_calculo;
+    });
+  }
+
+  void funcao_calcula_operacao_matematica() {
+    funcao_atualiza_displays();
+    variavel_exibe_calculo = variavel_exibe_calculo.replaceAll('x', '*');
+    try {
+      variavel_exibe_resultado_do_calculo =
+          funcao_eval(variavel_exibe_calculo).toString();
+    } catch (erro) {
+      print(erro);
+    }
+    variavel_exibe_calculo = variavel_exibe_calculo.replaceAll('*', 'x');
+    funcao_atualiza_displays();
+  }
+
+  void funcao_recebe_entradas_das_teclas(String entrada_tecla) {
+    if (lista_termos_operacionais.contains(entrada_tecla) == true) {
+      variavel_exibe_calculo += entrada_tecla;
+    }
+    if (entrada_tecla == '<x') {
+      variavel_exibe_calculo = variavel_exibe_calculo.substring(
+          0, variavel_exibe_calculo.length - 1);
+    }
+    if (entrada_tecla == 'AC') {
+      variavel_exibe_calculo = '';
+      variavel_exibe_resultado_do_calculo = '';
+    }
+    if (entrada_tecla == '=') {
+      funcao_calcula_operacao_matematica();
+      variavel_exibe_calculo = variavel_exibe_resultado_do_calculo;
+      variavel_exibe_resultado_do_calculo = '';
+      funcao_atualiza_displays();
+    }
+    if (entrada_tecla != '=') {
+      funcao_calcula_operacao_matematica();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(variavel_exibe_calculo,
+                    style: const TextStyle(fontSize: 50)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(variavel_exibe_resultado_do_calculo,
+                    style: const TextStyle(fontSize: 30)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('AC');
+                  },
+                  child: const Text('AC', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('(');
+                  },
+                  child: const Text('(', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas(')');
+                  },
+                  child: const Text(')', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('<x');
+                  },
+                  child: const Text('<x', style: TextStyle(fontSize: 30)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('7');
+                  },
+                  child: const Text('7', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('8');
+                  },
+                  child: const Text('8', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('9');
+                  },
+                  child: const Text('9', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('/');
+                  },
+                  child: const Text('/', style: TextStyle(fontSize: 30)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('4');
+                  },
+                  child: const Text('4', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('5');
+                  },
+                  child: const Text('5', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('6');
+                  },
+                  child: const Text('6', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('x');
+                  },
+                  child: const Text('x', style: TextStyle(fontSize: 30)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('1');
+                  },
+                  child: const Text('1', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('2');
+                  },
+                  child: const Text('2', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('3');
+                  },
+                  child: const Text('3', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('-');
+                  },
+                  child: const Text('-', style: TextStyle(fontSize: 30)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('0');
+                  },
+                  child: const Text('0', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('.');
+                  },
+                  child: const Text('.', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('=');
+                  },
+                  child: const Text('=', style: TextStyle(fontSize: 30)),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    funcao_recebe_entradas_das_teclas('+');
+                  },
+                  child: const Text('+', style: TextStyle(fontSize: 30)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
