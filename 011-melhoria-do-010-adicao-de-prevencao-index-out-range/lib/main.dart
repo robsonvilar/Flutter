b16f37913ce57bfad96239ccde092901d77b8dcd
Robson Vilar <61123228+robsonvilar@users.noreply.github.com>
2024-04-07 13:32:34 -0300

    add 011

---
+++ 011-melhoria-do-010-adicao-de-prevencao-index-out-range/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaAppState extends State<PerguntaApp> {
+  int _perguntaSelecionada = 0;
+
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita ?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito ?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito ?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    },
+  ];
+
+  void _responder() {
+    if (temPerguntaSelecionda == true) {
+      setState(() {
+        _perguntaSelecionada += 1;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionda {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    List<String> respostas = temPerguntaSelecionda ? _perguntas[_perguntaSelecionada].cast()['respostas'] : [];
+    List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Perguntas',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: temPerguntaSelecionda ? Column(
+            children: <Widget>[
+              Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+              ...widgets,
+            ],
+          ) : null,
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+}
